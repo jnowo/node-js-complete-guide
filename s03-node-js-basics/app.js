049a69416216354fe45dafcbc12d7bcd2d35d1f7
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-19 17:53:57 +0200

    s03 - set node js server

---
+++ s03-node-js-basics/app.js
@@ -0,0 +1,14 @@
+//require can import any file, not just node modules
+const http = require('http');
+/*
+function rqListener(req, res) {
+
+}
+*/
+
+//we don't need to define the function rqListener, we can just pass it as an anonymous function
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+server.listen(3000);
