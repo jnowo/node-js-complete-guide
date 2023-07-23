c4bd72f704932e1dc44e1d20358e8309dcc181e8
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 00:34:26 +0200

    s05_63 - handling different routes

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-//next is a function which will be passed to anonymous function inside use
+/*//next is a function which will be passed to anonymous function inside use
 app.use((req, res, next) => {
   console.log('In the middleware!');
   //if we don't call next request will 'die'. Allows the request to continue to the next middleware in line
   next();
+});*/
+app.use('/', (req, res, next) => {
+  console.log('This always runs!');
+  next();
 });
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In another the middleware!');
+
+  //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
+  res.send('<h1>Add product page.</h1>');
+});
+app.use('/', (req, res, next) => {
   console.log('In another the middleware!');
 
   //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
