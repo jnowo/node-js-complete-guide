4e05717265121c132a6e2da6e7c497fe44c80e88
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 00:26:52 +0200

    s05_62 - start serwer with express app.listen

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -6,7 +5,7 @@ const app = express();
 //next is a function which will be passed to anonymous function inside use
 app.use((req, res, next) => {
   console.log('In the middleware!');
-  //if we don't call next request will 'die'. Alows the request to continue to the next middleware in line
+  //if we don't call next request will 'die'. Allows the request to continue to the next middleware in line
   next();
 });
 app.use((req, res, next) => {
@@ -16,6 +15,4 @@ app.use((req, res, next) => {
   res.send('<h1>Hello from Express.js!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
