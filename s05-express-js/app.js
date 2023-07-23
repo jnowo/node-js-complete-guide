0a19a8b8510025bd67cb3f64d819b2608f8a516f
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 00:17:42 +0200

    s05_60 - use middleware to process request, use 'next' to use next middleware

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -3,6 +3,17 @@ const express = require('express');
 
 const app = express();
 
+//next is a function which will be passed to anonymous function inside use
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  //if we don't call next request will 'die'. Alows the request to continue to the next middleware in line
+  next();
+});
+app.use((req, res, next) => {
+  console.log('In another the middleware!');
+
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
