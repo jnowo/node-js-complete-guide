43922142b32b099a6569c1da9c3c696e3a268c33
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 00:22:26 +0200

    s05_61 - send response with express 'send' method

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -12,6 +12,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('In another the middleware!');
 
+  //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
+  res.send('<h1>Hello from Express.js!</h1>');
 });
 
 const server = http.createServer(app);
