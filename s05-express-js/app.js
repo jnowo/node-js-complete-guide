02c62260a970343d9ec5b54359bad54dd2cbd924
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 01:25:21 +0200

    s05_66 - using express Router

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
+app.use(adminRoutes);
+app.use(shopRoutes);
 app.use(bodyParser.urlencoded({extended: false}));
 
 /*//next is a function which will be passed to anonymous function inside use
@@ -10,20 +15,5 @@ app.use((req, res, next) => {
   next();
 });*/
 
-app.use('/add-product', (req, res, next) => {
-  //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
-  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-  console.log('In another the middleware!');
-
-  //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
-  res.send('<h1>Hello from Express.js!</h1>');
-});
 
 app.listen(3000);
