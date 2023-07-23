2f25f3a46eda719cb7f4483c75a537ac01559617
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 01:12:04 +0200

    s05_64 - parsing incoming requests

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 /*//next is a function which will be passed to anonymous function inside use
 app.use((req, res, next) => {
@@ -8,15 +9,15 @@ app.use((req, res, next) => {
   //if we don't call next request will 'die'. Allows the request to continue to the next middleware in line
   next();
 });*/
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
-app.use('/add-product', (req, res, next) => {
-  console.log('In another the middleware!');
 
+app.use('/add-product', (req, res, next) => {
   //send method set headers automatically but we can also set it by hand with setHeader method to overwrite default one
-  res.send('<h1>Add product page.</h1>');
+  res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 app.use('/', (req, res, next) => {
   console.log('In another the middleware!');
