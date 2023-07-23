746f24ccc7d35d7ff7792397b7bb54e4d2f08f0b
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-23 15:58:11 +0200

    s04_43 - npm, nodemon

---
+++ s04-improved-dev-wkf-debug/app.js
@@ -0,0 +1,26 @@
+/*
+- node is running on Event Loop which is a loop that keeps on running as long as there are event listeners registered
+- it is always available and waiting for events to happen
+- to break out of the event loop, we can use process.exit()
+- createServer or req.on are examples of event listeners passed to function which
+  will be executed in the future (important!) node.js use this pattern a lot
+- it is call as Event Driven Architecture (EDA)
+*/
+
+
+//require can import any file, not just node modules
+const http = require('http');
+const routes = require('./routes');
+/*
+function rqListener(req, res) {
+}
+*/
+
+//we don't need to define the function rqListener, we can just pass it as an anonymous function
+const server = http.createServer((req, res) => {
+
+
+  // process.exit(); //this will exit the event loop
+});
+
+server.listen(3000);
