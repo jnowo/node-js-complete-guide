4c59d1c1f6ccf6ae32c956ead2980a2f45399556
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-24 13:23:38 +0200

    s05_75 - move styling to public folder, add static middleware to server static files

--- s05-express-js/app.js
+++ s05-express-js/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //filtering routes, only requests starting with /admin will be handled by adminRoutes
 app.use('/admin', adminRoutes);
