71b4fa7520e251caa37ca36e102d619b6af1a48c
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-25 19:06:14 +0200

    s06_81 - using pug as template engine

--- s06-dynamic_content_templates/app.js
+++ s06-dynamic_content_templates/app.js
@@ -5,6 +5,10 @@ const shopRoutes = require('./routes/shop');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- s06-dynamic_content_templates/routes/shop.js
+++ s06-dynamic_content_templates/routes/shop.js
@@ -7,9 +7,7 @@ const rootDir = require('../utils/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  //in this case, data is shared between all users, which is not good
-  console.log('shop.ja', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
