60e4ca615c9b08d638ae3887fb4176e356c44eee
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-28 15:54:59 +0200

    s06_82 - moving shop.html to pug template

--- s06-dynamic_content_templates/routes/admin.js
+++ s06-dynamic_content_templates/routes/admin.js
@@ -15,7 +15,7 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-  products.push({title: req.body.title});
+  products.push({title: req.body.title, price: '19.99'});
   res.redirect('/');
 });
 
