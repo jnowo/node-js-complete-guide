3546488a5cd116dbe84a5ea3f8e90075d595c41e
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-29 01:53:04 +0200

    s06_84 - move all views to pug

--- s06-dynamic_content_templates/routes/admin.js
+++ s06-dynamic_content_templates/routes/admin.js
@@ -7,11 +7,7 @@ const router = express.Router();
 
 const products = [];
 router.get('/add-product', (req, res, next) => {
-  //send method sets headers automatically, but we can also set it by hand with setHeader method to overwrite default one
-  /*
-    res.send('<form action="/admin/add-product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>');
-  */
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add product'});
 });
 
 router.post('/add-product', (req, res, next) => {
