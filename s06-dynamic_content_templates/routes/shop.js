caa4bfc1bf3ef13530612499bc261b7070640b09
Jakub Nowotnik <nomeczek@gmail.com>
2023-07-29 17:30:58 +0200

    s06_90 - handlebars engine finish

--- s06-dynamic_content_templates/routes/shop.js
+++ s06-dynamic_content_templates/routes/shop.js
@@ -8,7 +8,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, path: '/', pageTitle: 'Welcome to my Shop!', hasProducts: products.length > 0});
+  res.render('shop', {
+    prods: products,
+    path: '/',
+    pageTitle: 'Welcome to my Shop!',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
